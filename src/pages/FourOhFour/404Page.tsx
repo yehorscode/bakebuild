f47b2a5c4e21799f0f66f8468913dd6badcc82ce
Yehor <omelianenkoyehor@proton.me>
2026-07-01 17:30:01 +0200

    s

--- src/pages/FourOhFour/404Page.tsx
+++ src/pages/FourOhFour/404Page.tsx
@@ -3,7 +3,7 @@ export default function FourohfourPage() {
   return (
     <div className="flex-1 text-white font-jaro items-center justify-center flex-col flex">
       <img src={confused} />
-      <span className="text-9xl">404</span>
+      <span className="txtshdw text-9xl">404</span>
       <span>This page doesnt exist</span>
       <a className="underline" href="/">
         Go back home
